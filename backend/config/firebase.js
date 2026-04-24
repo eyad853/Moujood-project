6fd9f465337c62b116fb0c91f6e6683fcaeedb69
eyad853 <eyadmosa853@gmail.com>
2026-04-24 05:34:31 +0300

    push latest updates

--- backend/config/firebase.js
+++ backend/config/firebase.js
@@ -1,6 +1,6 @@
 import dotenv from 'dotenv'
 dotenv.config()
-import * as admin from "firebase-admin";
+import admin from 'firebase-admin'
 
 admin.initializeApp({
     credential:admin.credential.cert({
