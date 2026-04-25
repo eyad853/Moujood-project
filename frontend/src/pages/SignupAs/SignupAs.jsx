013f50e0f05f797a631ff09debbea1e95a6c4dc7
eyad853 <eyadmosa853@gmail.com>
2026-04-25 05:35:38 +0300

    modify signupas

--- frontend/src/pages/SignupAs/SignupAs.jsx
+++ frontend/src/pages/SignupAs/SignupAs.jsx
@@ -3,7 +3,7 @@ import {Link} from 'react-router-dom'
 import { useTranslation } from 'react-i18next'
 
 const SignupAs = () => {
-    const {t} = useTranslation()
+    const {t} = useTranslation("signupAs")
   return (
     <div className="flex flex-col py-15 h-screen justify-between items-center">
         {/* the logo */}
@@ -23,6 +23,11 @@ const SignupAs = () => {
             to={'/client_sign_up'}
             className="w-full rounded-md h-10 flex font-bold justify-center items-center transform hover:scale-105 transition-all duration-200 text-white bg-green-700">
                 {t("userBtn")}
+            </Link>
+            <Link 
+            to={'/client_sign_up'}
+            className="w-full rounded-md h-10 flex font-bold justify-center items-center transform hover:scale-105 transition-all duration-200 text-white bg-green-700">
+                {t("login")}
             </Link>
         </div>
         
