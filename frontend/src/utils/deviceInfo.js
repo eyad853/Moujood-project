8727a4e4b18daa9b63c7635841b95ea806da8b1e
eyad853 <eyadmosa853@gmail.com>
2026-04-25 07:03:50 +0300

    modify signupas

--- frontend/src/utils/deviceInfo.js
+++ frontend/src/utils/deviceInfo.js
@@ -2,7 +2,7 @@ import { Preferences } from '@capacitor/preferences';
 import { Device } from '@capacitor/device';
 
 export const getDeviceInfo = async () => {
-  const deviceToken = (await Preferences.get({ key: 'pushToken' }))?.value;
+  const deviceToken = (await Preferences.get({ key: 'pushToken' }));
   const deviceId = (await Device.getId()).identifier;
   return { deviceToken, deviceId };
 };
